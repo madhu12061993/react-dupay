e192a5e1bf90b79612fbca8d3f4c474f09460e05
MadhuBala <smadhubala101@gmail.com>
2026-05-02 13:48:34 -0500

    navBar css addad

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css'
 import Banner from './component/Banner'
-import ExploreDupay from './component/ExploreDupay'
-import Factor from './component/Factor'
-import Flag from './component/Flag'
-import Footer from './component/Footer'
-import Future from './component/Future'
-import InternationalMoneyTransfer from './component/InternationalMoneyTransfer'
-import MoneyTransfer from './component/MoneyTransfer'
+// import ExploreDupay from './component/ExploreDupay'
+// import Factor from './component/Factor'
+// import Flag from './component/Flag'
+// import Footer from './component/Footer'
+// import Future from './component/Future'
+// import InternationalMoneyTransfer from './component/InternationalMoneyTransfer'
+// import MoneyTransfer from './component/MoneyTransfer'
 import Navbar from './component/Navbar'
-import PaymentsWay from './component/PaymentsWay'
-import Trust from './component/Trust'
+// import PaymentsWay from './component/PaymentsWay'
+// import Trust from './component/Trust'
 
 function App() {
 
@@ -17,15 +17,15 @@ function App() {
     <>
       <Navbar />
       <Banner />
-      <MoneyTransfer />
-      <PaymentsWay />
-      <Trust />
-      <Factor />
-      <InternationalMoneyTransfer />
-      <Flag/>
-      <ExploreDupay />
-      <Future />
-      <Footer />
+      {/* <MoneyTransfer /> */}
+      {/* <PaymentsWay /> */}
+      {/* <Trust /> */}
+      {/* <Factor /> */}
+      {/* <InternationalMoneyTransfer /> */}
+      {/* <Flag/> */}
+      {/* <ExploreDupay /> */}
+      {/* <Future /> */}
+      {/* <Footer /> */}
 
     </>
   )
