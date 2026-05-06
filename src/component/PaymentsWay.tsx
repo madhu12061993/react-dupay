a729d7283f6e20348a7c61c748189668ab937940
MadhuBala <smadhubala101@gmail.com>
2026-05-06 12:26:44 -0500

    changed line height in paymentsWay

--- src/component/PaymentsWay.tsx
+++ src/component/PaymentsWay.tsx
@@ -24,12 +24,12 @@ const PaymentsWay = () => {
           <h5 className="card-title headding">Your payments, <br/> your way.</h5>
           <p className="card-text subHeading">Get your very own du Pay card</p>
           <div className="col-5">
-            <div className="row row-cols-md-2  gx-3 cardBox">
+            <div className="row row-cols-md-2 gx-3 cardBox">
               {pament.map((item, index) => (
                 <div className="col" key={index}>
 
                   <div className="card h-100 " >
-                    <p className="card-title px-3 py-2">{item.text}</p>
+                    <p className="card-title px-3 pt-2 pb-1">{item.text}</p>
                   </div>
                 </div>
 
