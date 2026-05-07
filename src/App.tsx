2218d3fe1f7879c7ca78822cc8ee3e8869deafde
MadhuBala <smadhubala101@gmail.com>
2026-05-07 16:11:37 -0500

    something will be changed in moneyTransfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-// import Banner from './component/Banner'
+import Banner from './component/Banner'
 import ExploreDupay from './component/ExploreDupay'
 import Factor from './component/Factor'
 import Flag from './component/Flag'
 import Footer from './component/Footer'
 import Future from './component/Future'
 import InternationalMoneyTransfer from './component/InternationalMoneyTransfer'
-// import MoneyTransfer from './component/MoneyTransfer'
+import MoneyTransfer from './component/MoneyTransfer'
 import Navbar from './component/Navbar'
 import PaymentsWay from './component/PaymentsWay'
 import Trust from './component/Trust'
@@ -16,8 +16,8 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Banner /> */}
-      {/* <MoneyTransfer /> */}
+      <Banner />
+      <MoneyTransfer />
       <PaymentsWay />     
       <Trust />
       <hr className='mt-5'/>
--- src/component/MoneyTransfer.tsx
+++ src/component/MoneyTransfer.tsx
@@ -9,9 +9,11 @@ import "../css/MoneyTransfer.css";
 
 const MoneyTransfer = () => {
   return (
-    <div className="container d-flex flex-column align-items-center  mt-5 offer ">
-      <p className="heading">Here's what we offer</p>
-      <div className="row row-cols-md-2 d-flex justify-content-center">
+    <div className="container  mt-5 offer ">
+      <div className="headingParnt">
+        <p className="heading">Here's what we offer</p>
+      </div>
+      <div className="d-flex flex-row justify-content-center">
         {/* 1 col */}
         <div className="col-4 cardImage">
           <div className="card  ">
@@ -22,17 +24,19 @@ const MoneyTransfer = () => {
         <div className="col-4">
           <div className="card h-100">
             <div className="image">
-              <img src={svgAX}  alt="svgAX" />
+              <img src={svgAX} alt="svgAX" />
             </div>
             <h5 className="card-title">A multilingual experience</h5>
             <p className="card-text">Language barriers are a thing of the past. Navigate our app in Arabic, English, Hindi, Malayalam, Tamil and Bangla.</p>
           </div>
         </div>
+      </div>
+           <div className="d-flex flex-row justify-content-center">
         {/* 3 col */}
         <div className="col-4">
           <div className="card h-100">
             <div>
-            <img src={global}  alt="global" />
+              <img src={global} alt="global" />
             </div>
             <h5 className="card-title">Instant money transfer</h5>
             <p className="card-text"> Send money right away, anytime to more than 200 destinations around the world, from your phone. It’s fast, easy, and always there when you need it the most.</p>
@@ -44,17 +48,19 @@ const MoneyTransfer = () => {
             <img src={mobileChat} className="card-img-top" alt="mobileChat" />
           </div>
         </div>
+      </div>
+            <div className="d-flex flex-row justify-content-center">
         {/* 5 col */}
         <div className="col-4">
           <div className="card h-100">
             <img src={mobilepayment} className="card-img-top" alt="mobilepayment" />
           </div>
-        </div> 
+        </div>
         {/* 6 col */}
-         <div className="col-4">
+        <div className="col-4">
           <div className="card h-100">
             <div>
-            <img src={mobile}  alt="mobile" />
+              <img src={mobile} alt="mobile" />
             </div>
             <h5 className="card-title">Payments made simple</h5>
             <p className="card-text">Simplify your everyday life with our app. Effortlessly pay your bills or top-up your mobile in just few steps.</p>
@@ -66,3 +72,5 @@ const MoneyTransfer = () => {
 }
 
 export default MoneyTransfer
+
+
