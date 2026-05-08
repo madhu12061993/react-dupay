be03f5f5847ea334a98a2c76cb2da18e06dd1e49
MadhuBala <smadhubala101@gmail.com>
2026-05-08 13:20:52 -0500

    finished scroll

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react"
 import Fevicon from "../assets/dupayAssets/Logo.png"
+import dupayScroll from "../assets/dupayAssets/dupa logo scroll.png";
 import "../css/Navbar.css"
 
 const Navbar = () => {
+  const [isScroll, setisScroll] = useState(false);
+  useEffect (() => {
+    const handleScroll = () => {
+      setisScroll(window.scrollY > 50);
+    };
+    window.addEventListener("scroll" , handleScroll);
+    return ()=> {
+      window.removeEventListener("scroll" , handleScroll);
+    };
+  },[]);
   return (
     <div className="bg-warning">
-      <nav className="navbar navbar-light nav-color" >
+      <nav className={`navbar navbar-light navbar-expand-lg fixed-top ${isScroll ? " navColor " : "nav-color"}`} >
         <div className="container">
+          {isScroll ? (
+          <img src={dupayScroll} alt="DuePay" className="dupayScroll" />
+          ) : (
           <img src={Fevicon} alt="DuePay" className="duepayLogo" />
+          )}
           <form className="d-flex gap-4 me-4">
-            <div className="NavContact ">
-              <a href="#" className="text-white text-decoration-none fw-900">About Us</a>
-              <a href="#" className="text-white text-decoration-none fw-900">Contact Us</a>
+            <div className="NavContact">
+              <a href="#" className={` text-decoration-none fw-900 ${isScroll ? "navContactColor" : "text-white"}`}>About Us</a>
+              <a href="#" className={` text-decoration-none fw-900 ${isScroll ? "navContactColor" : "text-white"}`}>Contact Us</a>
             </div>
             <button className="btn  navBtn  p-2  px-4 border-2 btn-primary " type="submit">Download App</button>
           </form>
